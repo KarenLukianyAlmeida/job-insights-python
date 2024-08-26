8de4d1970f91c3f53827507384822172a46880df
Karen Almeida <karenalmeida.lukiany@gmail.com>
2024-08-26 18:12:42 +0100

    Feat: req. 2 (1.1)

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -1,12 +1,20 @@
 from typing import List, Dict
+import csv 
 
 
 class ProcessJobs:
     def __init__(self) -> None:
         self.jobs_list = list()
 
-    def read(self) -> List[Dict]:
-        pass
+    def read(self, path: str) -> List[Dict]:
+        with open(path, mode="r", encoding="utf8") as file:
+            jobs_reader = csv.DictReader(file, delimiter= ",")
+
+            for row in jobs_reader:
+                self.jobs_list.append(row)
+        
+        return self.jobs_list   
+    
 
     def get_unique_job_types(self) -> List[str]:
         pass
